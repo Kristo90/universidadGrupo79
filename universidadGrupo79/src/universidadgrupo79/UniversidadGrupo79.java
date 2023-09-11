6777e5c604def7fb4184a4e622d423cec06459e4
Kristo90 <96153349+Kristo90@users.noreply.github.com>
2023-09-11 00:04:47 -0300

    segundo commit

---
+++ universidadGrupo79/src/universidadgrupo79/UniversidadGrupo79.java
@@ -0,0 +1,17 @@
+
+package universidadgrupo79;
+
+/**
+ *
+ * @author kristo
+ */
+public class UniversidadGrupo79 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
