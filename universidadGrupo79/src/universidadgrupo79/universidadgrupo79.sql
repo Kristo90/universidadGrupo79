6be430f31e5dd55db42d4ee6ade0d86357df7c14
Guillermo With <guillermo.with@gmail.com>
2023-10-09 17:19:03 -0300

    GuilleWith agrego prueba de commit 09/10

--- universidadGrupo79/src/universidadgrupo79/universidadgrupo79.sql
+++ universidadGrupo79/src/universidadgrupo79/universidadgrupo79.sql
@@ -6,7 +6,7 @@
 -- Tiempo de generación: 11-09-2023 a las 23:41:06
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
-
+//esto es una prueba para ver si puedo hacer commit y push
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
